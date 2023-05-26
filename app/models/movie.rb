dded6ee7adea0fc7c96648d7f814511bea828a91
KEVIN MANLANGIT <kevinmanlangit24@gmail.com>
2023-05-26 11:51:42 +0100

    Rails-watch-list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates_presence_of :overview
+  validates :overview, presence: true
   validates :title, presence: true, uniqueness: true
 end
